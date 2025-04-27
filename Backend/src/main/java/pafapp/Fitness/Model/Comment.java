7da5346c8d62eaaf7575e07a8beb6f9180c9a278
Kobiram11 <thiyagarajahkobiram@gmail.com>
2025-04-27 14:26:22 +0530

    Comment model error resolve

--- Backend/src/main/java/pafapp/Fitness/Model/Comment.java
+++ Backend/src/main/java/pafapp/Fitness/Model/Comment.java
@@ -1,26 +1,16 @@
 package pafapp.Fitness.Model;
 
-import java.sql.Date;
-
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.Date;
 
 @Entity
 @Table(name = "comments")
