2735d64eb5ff1c7d4058770e284e330a83c102d3
Kobiram11 <thiyagarajahkobiram@gmail.com>
2025-04-27 14:02:15 +0530

    Comment repo is push by kobiram

---
+++ Backend/src/main/java/pafapp/Fitness/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package pafapp.Fitness.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pafapp.Fitness.Model.Comment;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
+
