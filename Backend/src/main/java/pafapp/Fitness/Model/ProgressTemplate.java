c1c486cbb001c01cd4a689914c53c6ac286a062c
Kobiram11 <thiyagarajahkobiram@gmail.com>
2025-04-27 15:05:27 +0530

    Progress template push by savidiya

---
+++ Backend/src/main/java/pafapp/Fitness/Model/ProgressTemplate.java
@@ -0,0 +1,12 @@
+package pafapp.Fitness.Model;
+
+public enum ProgressTemplate {
+
+    COMPLETED_5KM_RUN,
+    LIFTED_50KG,
+    COMPLETED_10KM_RUN,
+    LIFTED_100KG,
+    COMPLETED_YOGA_SESSION,
+    COMPLETED_SWIMMING_SESSION
+}
+
